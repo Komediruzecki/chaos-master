bc05bf39884406be355f88606e352fc3a6d49359
Komediruzecki <komediruzecki.2015@gmail.com>
2026-04-23 05:30:06 +0000

    Fix WebGPU buffer destruction error
    - Removed premature destruction of flameUniformsBuffer and outputTextureDimensionBuffer
    - These buffers are temporary and will be garbage collected when the component unmounts
    - Prevents 'used while destroyed' validation errors from WebGPU
    
    Resolves buffer cleanup issue with WebGPU rendering pipeline.

--- packages/app/src/flame/ifsPipeline.ts
+++ packages/app/src/flame/ifsPipeline.ts
@@ -1,4 +1,3 @@
-import { onCleanup } from 'solid-js'
 import { tgpu } from 'typegpu'
 import { arrayOf, builtin, f32, i32, struct, u32, vec2f, vec2i, vec2u, } from 'typegpu/data'
 import { add, arrayLength, atomicAdd, mul } from 'typegpu/std'
@@ -68,11 +67,6 @@ export function createIFSPipeline(
   const outputTextureDimensionBuffer = root
     .createBuffer(vec2i, vec2i(...outputTextureDimension))
     .$usage('uniform')
-
-  onCleanup(() => {
-    flameUniformsBuffer.destroy()
-    outputTextureDimensionBuffer.destroy()
-  })
 
   const bindGroup = root.createBindGroup(bindGroupLayout, {
     pointRandomSeeds,
